1a98788a396f29f3ddcf33efd5a6edd96dec3d0e
Vaibhav Bhardwaj <ajaykumars.9590@gmail.com>
2021-11-08 19:43:25 +0530

    networkList

--- src/modules/network/networkList.js
+++ src/modules/network/networkList.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import Header from '../header/header'
+import Sidebar from '../sidebar/sidebar'
+import { Row, Column } from 'simple-flexbox'
 
 const MainContainer = styled.div`
 background: #ECF0F7 0% 0% no-repeat padding-box;
 opacity: 1;
 width: 100%;
-height: 750px;
+height: 700px;
+`
+
+const HeaderContainer = styled.div`
+margin: 40px;
+width: 100%;
 `
 
 const Heading = styled.span`
 font-size: 20px;
+margin: 40px 0px 0px 8px;
 `
 const AddNetworkButton = styled.button`
 background: #3163F0 0% 0% no-repeat padding-box;
@@ -20,25 +29,36 @@ border: none;
 color: #ffffff;
 align-items: right;
 border-radius: 4px;
+position: absolute;
+right: 51px;
 `
 
 const Card = styled.div`
 background-color: #ffffff;
 border-radius: 4px;
-width: 100%;
 height: 400px;
-padding:40px;
+margin:50px;
 `
 
 function NetworkList() {
     return (
-        <MainContainer>
-            <Heading>Network</Heading>
-            <AddNetworkButton>+ Add Network</AddNetworkButton>
-            <Card>
+        <>
+            <Column>
+                <Header />
+                <Row>
+                    <Sidebar />
+                    <MainContainer>
+                        <HeaderContainer>
+                            <Heading><b>Networks</b></Heading>
+                            <AddNetworkButton>+ Add Network</AddNetworkButton>
+                        </HeaderContainer>
+                        <Card>
 
-            </Card>
-        </MainContainer>
+                        </Card>
+                    </MainContainer>
+                </Row>
+            </Column>
+        </>
     )
 }
 
